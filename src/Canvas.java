7881b646bb1571f378c646fed02553ac6cc0ccb3
KrishWu <krishwu@outlook.com>
2024-06-03 12:56:48 -0700

    Hi Mr. Kinney, simplified directories.

---
+++ src/Canvas.java
@@ -0,0 +1,269 @@
+package src;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.*;
+
+public class Canvas extends JComponent {
+    //Create the fields that are public and static.
+    public static final int TICK_SPEED = 500;
+    public static final int SOFT_DROP_TICK_SPEED = 50;
+    public static final int REFRESH_SPEED = 1;
+
+    //Create the private fields for the drawing of the game.
+    private final int WIDTH;
+    private final int HEIGHT;
+    private Game game;
+    private boolean isGameOver;
+    private int newTickTimer;
+    private int currTickSpeed;
+
+    private Sound tetrisSound;
+    private Sound clearLineSound;
+    private Sound dropSound;
+    // private Sound rightSound;
+    // private Sound leftSound;
+
+    //Constructor for the canvas given the width and height.
+    public Canvas(int width, int height) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+        this.WIDTH = width;
+        this.HEIGHT = height;
+
+        //Create a new game object.
+        game = new Game();
+        isGameOver = false;
+        newTickTimer = 0;
+        currTickSpeed = TICK_SPEED;
+
+       tetrisSound = new Sound("./src/sound/tetris.wav");
+       tetrisSound.loop();
+       clearLineSound = new Sound("./src/sound/clearLine.wav");
+       dropSound = new Sound("./src/sound/drop.wav");
+    //    rightSound = new Sound("./src/sound/right.wav");
+    //    leftSound = new Sound("./src/sound/left.wav");
+
+        // (new Thread(new MediaPlayer("tetris.wav")).start();
+
+        //Update the screen.
+        repaint();
+
+        //Create a new timer listener for the ticks that updates the screen.
+        class TimerListener implements ActionListener {
+            public void actionPerformed(ActionEvent e) {
+                if (!isGameOver) {
+                    newTickTimer++;
+                    if (newTickTimer >= currTickSpeed * game.getSpeedMultiplier()) {
+                        game.nextFrame();
+                        isGameOver = game.isGameOver();
+                        newTickTimer = 0;
+                        // System.out.println("Tick");
+                    }
+                }
+                if (game.getIsLineRemovedSound()) {
+                    clearLineSound.play();
+                }
+                repaint();
+            }
+        }
+
+        //Creates an instance of the timer.
+        Timer t = new Timer(REFRESH_SPEED, new TimerListener());
+        t.start();
+
+        //Creates a listener for keyboard inputs.
+        class KeyboardListener implements KeyListener {
+            public void keyPressed(KeyEvent e) {
+                // Right arrow key
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    if (game.moveRight()) {
+                        // rightSound.play(); //Disabled because it was to much.
+                    }
+                }
+                // Left arrow key
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    if (game.moveLeft()) {
+                        // leftSound.play(); //Disabled because it was too much.
+                    }
+                }
+                // Down arrow key
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    currTickSpeed = SOFT_DROP_TICK_SPEED;
+                }
+                // Up arrow key
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    game.rotateRight();
+                }
+            }
+
+            public void keyReleased(KeyEvent e) {
+                // Down arrow key
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    currTickSpeed = TICK_SPEED;
+                }
+
+            }
+
+            public void keyTyped(KeyEvent e) {
+                if (e.getKeyChar() == ' ') {
+                    if (game.drop()) {
+                        dropSound.play();
+                    };
+                }
+                if (e.getKeyChar() == 'r') {
+                    game = new Game();
+                    isGameOver = false;
+                }
+                if (e.getKeyChar() == 'z') {
+                    game.rotateLeft();
+                }
+                if (e.getKeyChar() == 'x') {
+                    game.rotateRight();
+                }
+                if (e.getKeyChar() == 'p') {
+                    game.pause();
+                }
+            }
+        }
+
+        addKeyListener(new KeyboardListener());
+        setFocusable(true);
+        requestFocus();
+    }
+
+    //Draw the board on the screen.
+    public void drawBoard(int[][] board, Graphics g) {
+        //Loops through the board rows.
+        for (int i = 0; i < board.length; i++) {
+            //Loops through the board columns.
+            for (int j = 0; j < board[i].length; j++) {
+                //Draws the various colors based off their number value.
+                if (board[i][j] == 0) {
+                    g.setColor(Color.BLACK);
+                } else if (board[i][j] == 1) {
+                    g.setColor(Color.RED);
+                } else if (board[i][j] == 2) {
+                    g.setColor(Color.ORANGE);
+                } else if (board[i][j] == 3) {
+                    g.setColor(Color.YELLOW);
+                } else if (board[i][j] == 4) {
+                    g.setColor(Color.GREEN);
+                } else if (board[i][j] == 5) {
+                    g.setColor(new Color(8, 240, 240));
+                } else if (board[i][j] == 6) {
+                    g.setColor(Color.BLUE);
+                } else if (board[i][j] == 7) {
+                    g.setColor(Color.PINK);
+                } else if (board[i][j] == 8) {
+                    g.setColor(new Color(255, 0, 0, 127));
+                } else if (board[i][j] == 9) {
+                    g.setColor(new Color(255, 165, 0, 127));
+                } else if (board[i][j] == 10) {
+                    g.setColor(new Color(255, 255, 0, 127));
+                } else if (board[i][j] == 11) {
+                    g.setColor(new Color(0, 255, 0, 127));
+                } else if (board[i][j] == 12) {
+                    g.setColor(new Color(8, 240, 240, 127));
+                } else if (board[i][j] == 13) {
+                    g.setColor(new Color(0, 0, 255, 127));
+                } else if (board[i][j] == 14) {
+                    g.setColor(new Color(255, 192, 203, 127));
+                }
+                g.fillRect(j * 40, i * 40 + 100, 40, 40);
+            }
+        }
+    }
+
+    //Draws the next piece.
+    public void drawNextPiece(int[][] nextPiece, Graphics g) {
+        for (int i = 0; i < nextPiece.length; i++) {
+            for (int j = 0; j < nextPiece[i].length; j++) {
+                if (nextPiece[i][j] == 0) {
+                    g.setColor(new Color(40, 40, 40));
+                } else if (nextPiece[i][j] == 1) {
+                    g.setColor(Color.RED);
+                } else if (nextPiece[i][j] == 2) {
+                    g.setColor(Color.ORANGE);
+                } else if (nextPiece[i][j] == 3) {
+                    g.setColor(Color.YELLOW);
+                } else if (nextPiece[i][j] == 4) {
+                    g.setColor(Color.GREEN);
+                } else if (nextPiece[i][j] == 5) {
+                    g.setColor(new Color(8, 240, 240));
+                } else if (nextPiece[i][j] == 6) {
+                    g.setColor(Color.BLUE);
+                } else if (nextPiece[i][j] == 7) {
+                    g.setColor(Color.PINK);
+                }
+
+                g.fillRect(j * 40 + 440, i * 40 + 100, 40, 40);
+            }
+        }
+    }
+
+    //Draws the stuff.
+    public void paintComponent(Graphics g) {
+        //This is the board.
+        g.setColor(new Color(40, 40, 40));
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+        drawBoard(game.getBoard(), g);
+        drawNextPiece(game.getNextPiece(), g);
+
+        //This is the title.
+        g.setColor(Color.GREEN);
+        g.setFont(new Font("Arial", Font.BOLD, 50));
+        g.drawString("Krish Wu's Tetris!", 90, 70);
+
+        //This is the points label.
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Arial", Font.BOLD, 50));
+        g.drawString("Points:", 415, 370);
+
+        //This is the score.
+        if (game.getScore() == 0) {
+            g.setColor(Color.WHITE);
+            g.setFont(new Font("Arial", Font.BOLD, 50));
+            g.drawString("0", 490, 430);
+        } else {
+            g.setColor(Color.WHITE);
+            g.setFont(new Font("Arial", Font.BOLD, 50));
+            g.drawString(game.getScore() + "", 445, 430); 
+        }
+
+        //This is the instructions.
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Arial", Font.BOLD, 20));
+        g.drawString("← - Move Left", 415, 500);
+        g.drawString("→ - Move Right", 415, 540);
+        g.drawString("↓ - Soft Drop", 415, 580);
+        g.drawString("X / ↑ - Rotate Right", 415, 620);
+        g.drawString("Z - Rotate Left", 415, 660);
+        g.drawString("Space - Hard Drop", 415, 700);
+        g.drawString("R - Restart", 415, 740);
+        g.drawString("P - Pause", 415, 780);
+
+        //This is the game over layover.
+        if (isGameOver) {
+            g.setColor(new Color(0, 0, 0, 127));
+            g.fillRect(150, 360, 300, 150);
+            g.setColor(Color.RED);
+            g.setFont(new Font("Arial", Font.PLAIN, 50));
+            g.drawString("Game Over", 170, 425);
+            g.setFont(new Font("Arial", Font.PLAIN, 25));
+            g.drawString("Press R to play again.", 180, 475);
+        }
+
+        //This is the is paused lay over.
+        if (game.isPaused()) {
+            g.setColor(new Color(0, 0, 0, 127));
+            g.fillRect(150, 360, 300, 150);
+            g.setColor(Color.YELLOW);
+            g.setFont(new Font("Arial", Font.PLAIN, 50));
+            g.drawString("Paused", 210, 425);
+            g.setFont(new Font("Arial", Font.PLAIN, 25));
+            g.drawString("Press P to resume.", 190, 475);
+        }
+    }
+}
---
+++ src/GamePiece.java
@@ -0,0 +1,182 @@
+package src;
+public class GamePiece {
+    //The various blocks for the pieces, with numbers representing their corresponding colors.
+    public final int[][] iBlock = {
+            { 0, 0, 5, 0 },
+            { 0, 0, 5, 0 },
+            { 0, 0, 5, 0 },
+            { 0, 0, 5, 0 }
+    };
+    public final int[][] jBlock = {
+            { 0, 6, 0 },
+            { 0, 6, 0 },
+            { 6, 6, 0 }
+    };
+    public final int[][] lBlock = {
+            { 0, 2, 0 },
+            { 0, 2, 0 },
+            { 0, 2, 2 }
+    };
+    public final int[][] oBlock = {
+            { 3, 3 },
+            { 3, 3 }
+    };
+    public final int[][] sBlock = {
+            { 0, 0, 0 },
+            { 0, 4, 4 },
+            { 4, 4, 0 }
+    };
+    public final int[][] tBlock = {
+            { 0, 0, 0 },
+            { 7, 7, 7 },
+            { 0, 7, 0 }
+    };
+    public final int[][] zBlock = {
+            { 0, 0, 0 },
+            { 1, 1, 0 },
+            { 0, 1, 1 }
+    };
+    public static final String blocks = "ijlostz"; //Possible strings for the blocks.
+
+    private int[][] block; //Block array.
+    private String type; //The type of block that it is as a string.
+
+    //Constructor to make a new GamePiece with a random block.
+    public GamePiece() {
+        // System.out.println(blocks.substring((int)(Math.random()*7)).substring(0,1));
+        this(blocks.substring((int) (Math.random() * 7)).substring(0, 1));
+    }
+    
+    //Constructor to make a new game piece given a block.
+    public GamePiece(String piece) {
+        if (piece.equals("i")) {
+            block = iBlock.clone();
+        } else if (piece.equals("j")) {
+            block = jBlock.clone();
+        } else if (piece.equals("l")) {
+            block = lBlock.clone();
+        } else if (piece.equals("o")) {
+            block = oBlock.clone();
+        } else if (piece.equals("s")) {
+            block = sBlock.clone();
+        } else if (piece.equals("t")) {
+            block = tBlock.clone();
+        } else if (piece.equals("z")) {
+            block = zBlock.clone();
+        }
+
+        type = piece;
+    }
+
+    // Rotate matrix 90 degrees right
+    public void rotateRight(int[][] board, int x, int y) {
+        // Create a new temp 2D array.
+        int[][] temp = new int[block.length][block[0].length];
+        // Check if rotation is possible and copy to new location in temp.
+        for (int r = 0; r < temp.length; r++) {
+            for (int c = 0; c < temp[r].length; c++) {
+                if (block[block[0].length - c - 1][r] != 0) {
+                    if (r + y >= board.length || c + x >= board[0].length) {
+                        return;
+                    }
+                    if (c + x < 0) {
+                        return;
+                    }
+                    if (r + y >= 0 && board[r + y][c + x] != 0) {
+                        return;
+                    }
+                }
+                temp[r][c] = block[block[0].length - c - 1][r];
+            }
+        }
+        // Copy temp back to the block.
+        for (int r = 0; r < temp.length; r++) {
+            for (int c = 0; c < temp[r].length; c++) {
+                block[r][c] = temp[r][c];
+            }
+        }
+    }
+
+    public void rotateLeft(int[][] board, int x, int y) {
+        // Create a new temp 2D array.
+        int[][] temp = new int[block.length][block[0].length];
+        // Check if rotation is possible and copy to new location in temp.
+        for (int r = 0; r < temp.length; r++) {
+            for (int c = 0; c < temp[r].length; c++) {
+                if (block[block[0].length - c - 1][r] != 0) {
+                    if (r + y >= board.length || c + x >= board[0].length) {
+                        return;
+                    }
+                    if (c + x < 0) {
+                        return;
+                    }
+                    if (r + y >= 0 && board[r + y][c + x] != 0) {
+                        return;
+                    }
+                }
+                temp[r][c] = block[c][block.length - r - 1];
+            }
+        }
+        // Copy temp back to the block.
+        for (int r = 0; r < temp.length; r++) {
+            for (int c = 0; c < temp[r].length; c++) {
+                block[r][c] = temp[r][c];
+            }
+        }
+    }
+
+    //Check if the bottom row of the block's matrix is empty or not.
+    public boolean isBottomEmpty() {
+        for (int section : block[block.length - 1]) {
+            if (section != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //Check if the second to the bottom row of the block is empty or not for the long line piece.
+    public boolean isSecondToBottomEmpty() {
+        for (int section : block[block.length - 2]) {
+            if (section != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //Get the number of the row that contains the lowest row of the block given the column.
+    public int getLowestRow(int c) {
+        for (int r = block.length - 1; r >= 0; r--) {
+            if (block[r][c] != 0) {
+                return r;
+            }
+        }
+        return -1;
+    }
+
+    //Get the height of the block.
+    public int getHeight() {
+        return block.length;
+    }
+
+    //Get the width of the block.
+    public int getWidth() {
+        return block[0].length;
+    }
+
+    //Get the specific section of the block given the row and column.
+    public int getSection(int r, int c) {
+        return block[r][c];
+    }
+
+    //Get the type of the block.
+    public String getType() {
+        return type;
+    }
+
+    //Get the entire 2D matrix of the block.
+    public int[][] getBlock() {
+        return block;
+    }
+}
